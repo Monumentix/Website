67edf23ea17408f8b9f51067d19eba0776a2d473
Monumentix <brian@monumentixwebdesign.com>
2016-04-21 06:22:53 +0100

    ADDED: Some verbiage , some layout and color tweaks

--- website/_protected/frontend/assets/AppAsset.php
+++ website/_protected/frontend/assets/AppAsset.php
@@ -29,6 +29,7 @@ class AppAsset extends AssetBundle
 
     public $css = [
         'css/site.css',
+        'css/monumentix.css',
     ];
     public $js = [
     ];
--- website/_protected/frontend/views/site/index.php
+++ website/_protected/frontend/views/site/index.php
@@ -35,8 +35,6 @@ $this->title = Yii::t('app', Yii::$app->name);
             <img class="pull-left img-circle img-responsive img-thumbnail img-coder-logo-small" src="<?=$this->theme->baseUrl?>/images/coder.jpg" style="width:120px;">
             <p class="">Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu.</p>
             <p>In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo. Nullam dictum felis eu pede mollis pretium. Integer tincidunt. Cras dapibus. Vivamus elementum semper nisi. Aenean vulputate eleifend tellus. Aenean leo ligula, porttitor eu, consequat vitae, eleifend ac, enim. Aliquam lorem ante, dapibus in, viverra quis, feugiat a, tellus. Phasellus viverra nulla ut metus varius laoreet. Quisque rutrum. Aenean imperdiet. Etiam ultricies nisi vel augue. Curabitur ullamcorper ultricies nisi. Nam eget dui. Etiam rhoncus. Maecenas tempus, tellus eget condimentum rhoncus, sem quam semper libero, sit amet adipiscing sem neque sed ipsum. Nam quam nunc, blandit vel, luctus pulvinar,</p>
-
-            <p><a class="btn btn-default" href="http://www.yiiframework.com/doc/">Yii Documentation &raquo;</a></p>
         </div>
 
         <div class="col-lg-4">
@@ -104,67 +102,126 @@ $this->title = Yii::t('app', Yii::$app->name);
 
   <div id="techiuse" class="container-fluid ">
     <div class="container">
-          <div id="" class="row">
-              <div class="col-lg-12">
-                  <h3 class="text-center">Technology I Use</h3>
+        <div id="" class="row">
+            <div class="col-lg-12">
+                <h3 class="text-center">Technology I Use</h3>
 
-                  <div id="" class="row center-block">
+                <div id="" class="row center-block">
 
-                    <div class="col-xs-4 col-sm-4">
-                      <div class="row">
-                        <div class="col-sm-6 col-sm-offset-4">
-                          <img src="<?=$this->theme->baseUrl?>/images/aws_logo_web.png" class="center-block img img-responsive ">
-                        </div>
-                      </div>
-                      <div class="row">
-                        <div class="col-sm-6 col-sm-offset-4">
-                          <img src="<?=$this->theme->baseUrl?>/images/github.png" class="center-block img img-responsive ">
-                        </div>
+                  <div class="col-xs-4 col-sm-4">
+                    <div class="row">
+                      <div class="col-sm-6 col-sm-offset-4">
+                        <img src="<?=$this->theme->baseUrl?>/images/aws_logo_web.png" class="center-block img img-responsive ">
                       </div>
                     </div>
-
-                    <div class="col-xs-4 col-sm-4">
-                      <div class="row">
-                        <div class="col-xs-6 col-sm-8">
-                          <img src="<?=$this->theme->baseUrl?>/images/Tux.png" class="center-block img img-responsive ">
-                        </div>
-                        <div class="col-xs-6 col-sm-4">
-                          <img src="<?=$this->theme->baseUrl?>/images/php-logo.png" class="center-block img img-responsive ">
-                        </div>
-                      </div>
-                      <div class="row">
-                        <div class="col-xs-6 col-sm-8">
-                          <img src="<?=$this->theme->baseUrl?>/images/apache-logo.png" class="center-block img img-responsive ">
-                        </div>
-                        <div class="col-xs-6 col-sm-4">
-                          <img src="<?=$this->theme->baseUrl?>/images/MySQL.png" class="center-block img img-responsive ">
-                        </div>
+                    <div class="row">
+                      <div class="col-sm-6 col-sm-offset-4">
+                        <img src="<?=$this->theme->baseUrl?>/images/github.png" class="center-block img img-responsive ">
                       </div>
                     </div>
+                  </div>
 
-                    <div class="col-xs-4 col-sm-4">
-                      <div class="row">
-                        <div class="col-sm-10 col-sm-offset-2">
-                          <img src="<?=$this->theme->baseUrl?>/images/yii-logo.png" class="center-block img img-responsive ">
-                        </div>
+                  <div class="col-xs-4 col-sm-4">
+                    <div class="row">
+                      <div class="col-xs-6 col-sm-8">
+                        <img src="<?=$this->theme->baseUrl?>/images/Tux.png" class="center-block img img-responsive ">
                       </div>
-                      <div class="row">
-                        <div class="col-sm-10 col-sm-offset-2">
-                          <img src="<?=$this->theme->baseUrl?>/images/Magento-logo.png" class="center-block img img-responsive ">
-                        </div>
+                      <div class="col-xs-6 col-sm-4">
+                        <img src="<?=$this->theme->baseUrl?>/images/php-logo.png" class="center-block img img-responsive ">
                       </div>
                     </div>
+                    <div class="row">
+                      <div class="col-xs-6 col-sm-8">
+                        <img src="<?=$this->theme->baseUrl?>/images/apache-logo.png" class="center-block img img-responsive ">
+                      </div>
+                      <div class="col-xs-6 col-sm-4">
+                        <img src="<?=$this->theme->baseUrl?>/images/MySQL.png" class="center-block img img-responsive ">
+                      </div>
+                    </div>
+                  </div>
 
-                </div>
-
-
-
+                  <div class="col-xs-4 col-sm-4">
+                    <div class="row">
+                      <div class="col-sm-10 col-sm-offset-2">
+                        <img src="<?=$this->theme->baseUrl?>/images/yii-logo.png" class="center-block img img-responsive ">
+                      </div>
+                    </div>
+                    <div class="row">
+                      <div class="col-sm-10 col-sm-offset-2">
+                        <img src="<?=$this->theme->baseUrl?>/images/Magento-logo.png" class="center-block img img-responsive ">
+                      </div>
+                    </div>
+                  </div>
               </div>
-
-
-          </div>
-
+            </div>
+        </div>
       </div>
   </div>
+
+
+
+  <div id="thingsivedone" class="container-fluid ">
+    <div class="container">
+      <div id="" class="row">
+          <div class="col-lg-12">
+              <h3 class="text-center">Latest Project : <small class="lead">Scalable Cloud-Based E-Commerce Solution</small></h3>
+              <div class="row">
+                <div class="col-sm-6">
+
+                  <div class="row">
+                    <div class="col-sm-3">
+                      <img src="<?=$this->theme->baseUrl?>/images/aws_logo_web.png" class="center-block img img-responsive ">
+                    </div>
+                    <div class="col-sm-8">
+                      <h3>Cloud Based Scalable Infastructre</h3>
+                      <p>A scalable and cloud based option centered around Amazons AWS infastrucre was choosen in part to cost, as well as to the range of services that integrated well with our choosen ecommerce package.  The largest upside has been the ability to use GIT and Cloud Formation templates to deploy/manage our network stack from source code version controlled templates. </p>
+                    </div>
+                  </div>
+
+                  <div class="row">
+                    <div class="col-sm-3">
+                      <img src="<?=$this->theme->baseUrl?>/images/aws_logo_web.png" class="center-block img img-responsive ">
+                    </div>
+                    <div class="col-sm-8">
+                      <h3>Ecommerce Package</h3>
+                      <p>When it came time to choose an ecommerce solution while cumbersome and bloated at times, magento does a good job at what it does.  That is provide an open source ecommerce solution with a large supporting community of developers and modules to enhacen and extend the platform. </p>
+                    </div>
+                  </div>
+
+                  <div class="row">
+                    <div class="col-sm-3">
+                      <img src="<?=$this->theme->baseUrl?>/images/aws_logo_web.png" class="center-block img img-responsive ">
+                    </div>
+                    <div class="col-sm-8">
+                      <h3>Supporting Application</h3>
+                      <p>Custom Web Based Buisness Application to  streamline and automate day to day processes that were not feasible or suitable to existing sub-systems.  Initially this Application started life as an data entry for inventory, as well as inventory tracking applcation, but quickly expanded to fill many other roles.</p>
+                    </div>
+                  </div>
+
+
+                </div>
+                <div class="col-sm-6">
+                  <img id="searchlightcomics-screenshot" src="<?=$this->theme->baseUrl?>/images/searchlightcomics-screenshot-with-monitor.png" class="center-block img img-responsive ">
+                </div>
+              </div>
+
+              <div class="row">
+                <div class="col-sm-4 text-center">
+                  <a class="btn btn-lg btn-success shadow" href="#"><span class="glyphicon glyphicon-certificate"></span> Learn how it all works</a>
+                </div>
+                <div class="col-sm-4 text-center">
+                  <a class="btn btn-lg btn-primary shadow" href="#"><span class="glyphicon glyphicon-link"></span> See the storefront</a>
+                </div>
+                <div class="col-sm-4 text-center">
+                  <a class="btn btn-lg btn-warning shadow" href="#"><span class="glyphicon glyphicon-exclamation-sign"></span> Play with some modules</a>
+                </div>
+
+              </div>
+          </div>
+      </div>
+    </div>
+  </div>
+
+
 
 </div>
