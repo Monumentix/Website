1cb9f256960e230c5f2df1d95be8f17e8b1778c1
Monumentix <brian@monumentixwebdesign.com>
2016-04-21 23:55:31 +0100

    UPDATED: Blog section

---
+++ website/_protected/frontend/models/BlogSearch.php
@@ -0,0 +1,82 @@
+<?php
+namespace frontend\models;
+
+use yii\base\Model;
+use yii\data\ActiveDataProvider;
+use Yii;
+
+/**
+ * BlogSearch represents the model behind the search form about `app\models\Blog`.
+ */
+class BlogSearch extends Blog
+{
+    /**
+     * Returns the validation rules for attributes.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            [['id', 'user_id', 'status', 'created_at', 'updated_at'], 'integer'],
+            [['title', 'summary', 'content'], 'safe'],
+        ];
+    }
+
+    /**
+     * Returns a list of scenarios and the corresponding active attributes.
+     *
+     * @return array
+     */
+    public function scenarios()
+    {
+        // bypass scenarios() implementation in the parent class
+        return Model::scenarios();
+    }
+
+    /**
+     * Creates data provider instance with search query applied
+     *
+     * @param array   $params    The search query params.
+     * @param integer $pageSize  The number of results to be displayed per page.
+     * @param boolean $published Whether or not Blogs have to be published.
+     *
+     * @return ActiveDataProvider
+     */
+    public function search($params, $pageSize = 3, $published = false)
+    {
+        $query = Blog::find();
+
+        // this means that editor is trying to see Blogs
+        // we will allow him to see published ones and drafts made by him
+        if ($published === true)
+        {
+            $query->where(['status' => Blog::STATUS_PUBLISHED]);
+            $query->orWhere(['user_id' => Yii::$app->user->id]);
+        }
+
+        $dataProvider = new ActiveDataProvider([
+            'query' => $query,
+            'sort'=> ['defaultOrder' => ['id' => SORT_DESC]],
+            'pagination' => [
+                'pageSize' => $pageSize,
+            ]
+        ]);
+
+        if (!($this->load($params) && $this->validate())) {
+            return $dataProvider;
+        }
+
+        $query->andFilterWhere([
+            'id' => $this->id,
+            'user_id' => $this->user_id,
+            'status' => $this->status,
+        ]);
+
+        $query->andFilterWhere(['like', 'title', $this->title])
+              ->andFilterWhere(['like', 'summary', $this->summary])
+              ->andFilterWhere(['like', 'content', $this->content]);
+
+        return $dataProvider;
+    }
+}
