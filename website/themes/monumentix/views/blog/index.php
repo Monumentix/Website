fbaae5da47b14bd6f066ef4fd2a725a9fa1e8d59
Monumentix <brian@monumentixwebdesign.com>
2016-04-22 07:13:22 +0100

    ADDED: basic layout for blog page

--- website/themes/monumentix/views/blog/index.php
+++ website/themes/monumentix/views/blog/index.php
@@ -6,25 +6,35 @@ use yii\widgets\ListView;
 /* @var $searchModel app\models\ArticleSearch */
 /* @var $dataProvider yii\data\ActiveDataProvider */
 
-$this->title = Yii::t('app', Yii::$app->name) .' '. Yii::t('app', 'news');
+$this->title = Yii::t('app', 'Blog');
+
 $this->params['breadcrumbs'][] = Yii::t('app', 'Articles');
 ?>
-<div class="article-index">
-
-    <h1><?= Html::encode($this->title) ?>
-        <span class="small"> - <?= Yii::t('app', 'The best news available') ?></span>
+<div class="blog-index">
+    <h1>
+        <?= Html::encode($this->title) ?>
+        <span class="small"> - <?= Yii::t('app', 'Latest news and information') ?></span>
     </h1>
+    <hr class="top">
+
+    <div class="row">
+        <div class="col-md-10 col-md-offset-1">
+        <?= ListView::widget([
+            'summary' => false,
+            'dataProvider' => $dataProvider,
+            'emptyText' => Yii::t('app', 'We haven\'t created any blogs yet.'),
+            'itemOptions' => ['class' => 'item'],
+            'itemView' => function ($model, $key, $index, $widget) {
+                return $this->render('_index', ['model' => $model]);
+            },
+        ]) ?>
+        </div>
+        <div class="col-md-1">
+
+        </div>
+    </div>
 
     <hr class="top">
 
-    <?= ListView::widget([
-        'summary' => false,
-        'dataProvider' => $dataProvider,
-        'emptyText' => Yii::t('app', 'We haven\'t created any articles yet.'),
-        'itemOptions' => ['class' => 'item'],
-        'itemView' => function ($model, $key, $index, $widget) {
-            return $this->render('_index', ['model' => $model]);
-        },
-    ]) ?>
 
 </div>
